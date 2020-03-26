38233ff891ce96aa1c302ff8bc67750778ec8ce6
twiby <t.balenbois@gmail.com>
2020-03-26 20:22:59 +0100

    error rates mode and figures

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import argparse
 import numpy as np
 import matplotlib as mpl
@@ -6,16 +7,13 @@ import matplotlib.pyplot as plt
 
 def infect(nTot, infectionRate):
 	from random import shuffle, random
-	nInfected = int(nTot * infectionRate)
 	infectedIDs = [random()<=infectionRate for _ in range(nTot)]
 	assert(len(infectedIDs) == nTot)
 	shuffle(infectedIDs)
 	return infectedIDs
 
 
-
-
-def main(nIndividuals=1000000, show=False, build=False):
+def nTestsMode(nIndividuals, show, build):
 	import testingMachine as tm
 	from tqdm import tqdm
 
@@ -30,6 +28,9 @@ def main(nIndividuals=1000000, show=False, build=False):
 			infectedIndividuals = infect(nIndividuals, infectionRate)
 
 			results.append(tm.applyAllStrategies(infectedIndividuals, poolSizes, pbar=pbar))
+			plt.xlabel("pool size")
+			plt.ylabel("relative number of tests compared to individual testing")
+			plt.title("different pooling strategies tried to reduce total number of tests (infection rate "+str(np.sum(infectedIndividuals)/len(infectedIndividuals)*100)+"%)")
 			plt.savefig('./results_infected'+str(int(infectionRate*100))+'%.svg', format="svg")
 			plt.savefig('./results_infected'+str(int(infectionRate*100))+'%.png', format="png")
 			if show:
@@ -99,10 +100,93 @@ def main(nIndividuals=1000000, show=False, build=False):
 		plt.close()
 
 
+def errorRateMode(nIndividuals, show, build, falseNegativeRate):
+	import testingMachine as tm
+	from tqdm import tqdm
+
+	print("Testing all strats error rates with a false negative rate of",falseNegativeRate)
+
+	if not os.path.isfile("./results_errorRate"+str(falseNegativeRate)+".npz") or build:
+		poolSizes = [2,3,4,5,6,7,8,9,10,12,14,17,20,25,30,40,50,60,70,80,90,100]
+		infectionRates = list(np.array([i for i in range(1,30)] + [i for i in range(30,52,2)]) / 100)
+		results = []
+
+		pbar = tqdm(total=len(infectionRates)*len(poolSizes)*len(tm.getStratNames()))
+		for infectionRate in infectionRates:
+			infectedIndividuals = infect(nIndividuals, infectionRate)
+
+			results.append(tm.applyAllStrategies(infectedIndividuals, poolSizes, pbar=pbar, falseNegativeRate=falseNegativeRate))
+			plt.xlabel("pool size")
+			plt.ylabel("false negative rate")
+			plt.title("error rates for each pooling strategy (infection rate "+str(np.sum(infectedIndividuals)/len(infectedIndividuals)*100)+"%)")
+			plt.savefig('./results_errors'+str(falseNegativeRate)+'_infected'+str(int(infectionRate*100))+'%.svg', format="svg")
+			plt.savefig('./results_errors'+str(falseNegativeRate)+'_infected'+str(int(infectionRate*100))+'%.png', format="png")
+			if show:
+				plt.show()
+			else:
+				plt.close()
+		pbar.close()
+		results = np.array(results)
+		np.savez("./results_errorRate"+str(falseNegativeRate), results=results, rates=infectionRates, pools=poolSizes)
+
+	else:
+		r = np.load("./results_errorRate"+str(falseNegativeRate)+".npz", allow_pickle=True)
+		results = r["results"]
+		infectionRates = r["rates"]
+		poolSizes = r["pools"]
+
+	stratNames = tm.getStratNames()
+
+	### Color plots
+	xBoundaries = [(infectionRates[n+1]+infectionRates[n])/2 for n in range(len(infectionRates)-1)]
+	xBoundaries = [2*infectionRates[0] - xBoundaries[0]] + xBoundaries + [2*infectionRates[-1] - xBoundaries[-1]]
+	yBoundaries = [(poolSizes[n+1]+poolSizes[n])/2 for n in range(len(poolSizes)-1)]
+	yBoundaries = [2*poolSizes[0] - yBoundaries[0]] + yBoundaries + [2*poolSizes[-1] - yBoundaries[-1]]
+
+	xBoundaries, yBoundaries = np.meshgrid(xBoundaries, yBoundaries)
+
+	results = results.astype(float)
+	for n in range(len(infectionRates)):
+		results[n,:,:] = (results[n,:,:] / infectionRates[n]) / nIndividuals
+
+	fig = plt.figure(figsize=(15,9))
+	subp = [221,222,223,224]
+	for n in range(len(stratNames)):
+		fig.add_subplot(subp[n])
+		plt.pcolormesh(xBoundaries, yBoundaries, results[:,n,:].transpose())
+		plt.title('error rates ('+stratNames[n]+')', fontsize=15)
+		cbar = plt.colorbar()
+		cbar.set_label('relative error rates compared to individual testing', rotation=270, labelpad=20)
+		plt.xlabel('infectionRate')
+		plt.ylabel('pool size')
+	plt.savefig('./results_all_error'+str(falseNegativeRate)+'.svg', format="svg")
+	plt.savefig('./results_all_error'+str(falseNegativeRate)+'.png', format="png")
+	if show:
+		plt.show()
+	else:
+		plt.close()
+
+
+def main(nIndividuals=1000000, show=False, build=False, mode='', errorRate=0):
+	import warnings
+
+	if mode not in ['nTests','errorRate']:
+		raise ValueError("mode is not valid")
+	elif mode == 'nTests':
+		if errorRate > 0:
+			warnings.warn("while in nTests mode, the error rate is considered 0.", Warning)
+		nTestsMode(nIndividuals, show, build)
+	elif mode == 'errorRate':
+		errorRateMode(nIndividuals, show, build, errorRate)
+	
+
+
 if __name__=="__main__":
 	parser = argparse.ArgumentParser(description="Computing different tactics of testing covid")
+	parser.add_argument("mode", type=str, help="mode: nTests | errorRate")
 	parser.add_argument('-n','--nIndividuals', type=int, help="number of humans")
 	parser.add_argument('-s','--show', action='store_true', help="show figure")
 	parser.add_argument('-b','--build', action='store_true', help="rebuild results")
+	parser.add_argument('-e','--errorRate', type=float, help="false negative rate of the test")
 	args = {k:v for k,v in vars(parser.parse_args()).items() if v is not None}
 	main(**args)
--- testingMachine.py
+++ testingMachine.py
@@ -4,7 +4,7 @@ import multiprocessing
 import matplotlib as mpl 
 import matplotlib.pyplot as plt
 import testingStrategies as ts
-
+from random import random
 
 def pools(data, poolSize):
 	if not isinstance(data, list):
@@ -16,27 +16,47 @@ def pools(data, poolSize):
 			yield data[n*poolSize:(n+1)*poolSize]
 
 class Tests(object):
-	def __init__(self):
+	def __init__(self, falseNegativeRate=0):
 		self.nTests = 0
+		self.falseNegativeRate = falseNegativeRate
+		if self.falseNegativeRate > 0:
+			self.testErrors = True
+		else:
+			self.testErrors = False
 
 	def test(self, pool):
 		self.nTests += 1
+		if self.testErrors:
+			if random() < self.falseNegativeRate:
+				return False
+
 		if isinstance(pool, (list, np.ndarray)):
 			return np.any(pool)
 		else:
 			return pool
 
 
+def getStratErrorRate(strat, infectedIndividuals, poolSize, falseNegativeRate):
+	# print("testing error of ", strat.__doc__,"with pool ",poolSize)
+	testingMachine = Tests(falseNegativeRate=falseNegativeRate)
+	testedPositive = strat(infectedIndividuals, poolSize, testingMachine)
+	return np.sum([testedPositive[n]!=infectedIndividuals[n] for n in range(len(infectedIndividuals))])
 
-def applyTestingStrategy(strat, infectedIndividuals, poolSize):
+
+def getStratTestCount(strat, infectedIndividuals, poolSize, *args):
 	# print("Applying", strat.__doc__,"with pool ",poolSize)
 	testingMachine = Tests()
 	testedPositive = strat(infectedIndividuals, poolSize, testingMachine)
 	assert(np.any([testedPositive[n]!=infectedIndividuals[n] for n in range(len(infectedIndividuals))])==False)
 	return testingMachine.nTests
 
-def applyAllStrategies(infectedIndividuals, poolSizes, pbar=None):
+def applyAllStrategies(infectedIndividuals, poolSizes, pbar=None, falseNegativeRate=None):
 	plt.figure(figsize=(15,9))
+
+	if falseNegativeRate!=None:
+		mode = getStratErrorRate
+	else:
+		mode = getStratTestCount
 
 	results = []
 	for strat in ts.getAllStrats():
@@ -51,14 +71,11 @@ def applyAllStrategies(infectedIndividuals, poolSizes, pbar=None):
 			if group == []:
 				continue
 			with multiprocessing.Pool(len(group)) as p:
-				res += p.starmap(applyTestingStrategy, ((strat, infectedIndividuals, poolSize) for poolSize in group))
+				res += p.starmap(mode, ((strat, infectedIndividuals, poolSize, falseNegativeRate) for poolSize in group))
 			if pbar:
 				pbar.update(len(group))
 		results.append(res)
-		p = plt.plot(poolSizes, res, label=strat.__doc__+" (min: "+str(poolSizes[np.argmin(res)])+")")
-	plt.xlabel("poolSize")
-	plt.ylabel("number of tests performed")
-	plt.title("different pooling strategies tried to reduce total number of tests (infection rate "+str(np.sum(infectedIndividuals)/len(infectedIndividuals)*100)+"%)")
+		p = plt.plot(poolSizes, np.array(res)/len(infectedIndividuals), label=strat.__doc__+" (min: "+str(poolSizes[np.argmin(res)])+")")
 	plt.legend(loc="lower right")
 
 	return results
